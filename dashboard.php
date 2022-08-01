8b240afd942968b859faaa8c5ed31c6eae89b094
Joel <89223729+joel-sage@users.noreply.github.com>
2022-08-01 19:00:15 +0100

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -17,8 +17,8 @@ if (isset($_GET['logout']) && $_GET['logout'] == 'true') {
     <meta http-equiv="X-UA-Compatible" content="IE=edge" user="JOEL-SAGE">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="pro/css/all.css">
-    <title>TO DO APPLICATION SAGE</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
+    <title>TO DO APPLICATION FROM SAGE</title>
 </head>
 
 <body>
